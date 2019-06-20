36dbe65056f4fa327ce63b2f318532457939a30c
Aakarsh Chopra <alcatrazz.627@gmail.com>
2019-06-20 08:49:31 +0530

    Init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './components/App';
+
+import './styles/global.scss';
+// import '../node_modules/bulma/bulma.sass';
+
+ReactDOM.render(<App />, document.getElementById('root'))
