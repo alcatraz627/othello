1811b8067438d4fa3b0587638f46a34521d5fa3d
Aakarsh Chopra <alcatrazz.627@gmail.com>
2019-06-20 10:09:35 +0530

    Basic board built

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React, {useState} from 'react'
+import _ from 'lodash'
+
+const DIMENSION = 8
+
+const COLORS = {
+    BLACK: 'BLACK', 
+    WHITE: 'WHITE'
+}
+
+const BOX_STATES = {
+    EMPTY: 'EMPTY',
+    WALL: 'WALL',
+    [COLORS.BLACK]: COLORS.BLACK,
+    [COLORS.WHITE]: COLORS.WHITE,
+}
+
+const INIT_POS = [
+    {
+        x: DIMENSION/2 - 1,
+        y: DIMENSION/2 - 1,
+        color: COLORS.BLACK
+    },
+    {
+        x: DIMENSION/2,
+        y: DIMENSION/2,
+        color: COLORS.BLACK
+    },
+    {
+        x: DIMENSION/2 - 1,
+        y: DIMENSION/2,
+        color: COLORS.WHITE
+    },
+    {
+        x: DIMENSION/2,
+        y: DIMENSION/2 - 1,
+        color: COLORS.WHITE
+    },
+]
+
+const createBoard = () => {
+    var INITIAL_BOARD = _.times(DIMENSION, ()=>(_.times(DIMENSION, ()=>BOX_STATES.EMPTY)));
+
+    INIT_POS.map(({x, y, color}) => {
+        // console.log(x, y, color);
+        INITIAL_BOARD[x][y] = color;
+    })
+    
+    return INITIAL_BOARD;
+
+}
+
 
 const App = () => {
+    const [Board, setBoard] = useState(createBoard())
+
     return (
-        <div>
-            <h3>Henlo</h3>
+        
+        <div className="section">
+            <div className="container text-center">
+                <div className="title">Othello Game</div>
+            <div className="board">
+
+                {/* {JSON.stringify(Board)} */}
+
+                {Board.map((col, x) => 
+                    <div className="row">
+                        {col.map((cell, y) => 
+                        <div className="cell">
+                            <div className={`cont state-${cell.toLowerCase()}`}>
+                                {`${x+1}, ${y+1}`}
+                            </div>
+                        </div>
+                        )}
+                    </div>
+                )}
+            </div>
+            </div>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,5 @@ import ReactDOM from 'react-dom'
 import App from './components/App';
 
 import './styles/global.scss';
-// import '../node_modules/bulma/bulma.sass';
 
 ReactDOM.render(<App />, document.getElementById('root'))
